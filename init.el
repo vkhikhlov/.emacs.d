e6483121b6548a73cf94f3df9517f8aa89daadf1
vkhikhlov <hihlow.vladislav@yandex.ru>
2021-07-31 01:33:29 +0300

    1. backups disabled and autosave enabled 2. paren and elec-pair built-in modes activated 3. undo and redo keybindings added (with help of undo-fu package) 4. some minor changes and settings

--- init.el
+++ init.el
@@ -17,18 +17,36 @@
 (blink-cursor-mode 0)
 (column-number-mode t)
 (load-theme 'tsdh-light)
+(fset 'yes-or-no-p 'y-or-n-p)
 (global-display-line-numbers-mode)
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 
+(auto-save-visited-mode 
+ (setq auto-save-visited-interval 0.1))
+
 (setq display-time-24hr-format t)
 (setq tab-always-indent 'complete)
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq create-lockfiles nil) ; stop creating .# files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-verbose t)
+
+;; built-in packages
+(use-package paren
+  :config
+  (show-paren-mode +1))
+
+(use-package elec-pair
+  :config
+  (electric-pair-mode +1))
 
 (use-package windmove
   :config
@@ -79,6 +97,13 @@
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 
+(use-package undo-fu
+  :ensure t)
+
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "C-z") 'undo-fu-only-undo)
+(global-set-key (kbd "C-S-z") 'undo-fu-only-redo)
+
 (use-package which-key
   :ensure t
   :config
@@ -109,7 +134,7 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0.5)
+  (setq company-idle-delay 0.1)
   (setq company-show-numbers t)
   (setq company-tooltip-limit 10)
   (setq company-minimum-prefix-length 2)
